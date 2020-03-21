9a8e35ad154326da001406e1e508f8db505113a3
Kagank <kagan-kuscu@hotmail.com.tr>
2020-03-21 14:22:55 +0200

    Test

--- STA.Backend.API/src/main/java/com/Feelfree2code/STA/controller/HomeController.java
+++ STA.Backend.API/src/main/java/com/Feelfree2code/STA/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "com.Feelfree2code.STA")
-@RestController
+@RestController 
 public class HomeController {
 
 	private final MyService myService;
