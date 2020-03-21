7cf65417c40c1239ecbf701cf550f59eb4a99638
Kagank <kagan-kuscu@hotmail.com.tr>
2020-03-21 11:56:28 +0200

    First app created multi module.

---
+++ STA.Backend.API/src/main/java/com/Feelfree2code/STA/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.Feelfree2code.STA.controller;
+
+import com.Feelfree2code.STA.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.Feelfree2code.STA")
+@RestController
+public class HomeController {
+
+	private final MyService myService;
+
+	public HomeController(MyService myService) {
+		this.myService = myService;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return myService.message();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeController.class, args);
+	}
+}
---
+++ STA.Backend.Data/src/main/java/com/Feelfree2code/STA/service/MyService.java
@@ -0,0 +1,24 @@
+package com.Feelfree2code.STA.service;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+// @Service
+// public class Service {
+
+// }
+
+@Service
+@EnableConfigurationProperties(ServiceProperties.class)
+public class MyService {
+
+	private final ServiceProperties serviceProperties;
+
+	public MyService(ServiceProperties serviceProperties) {
+		this.serviceProperties = serviceProperties;
+	}
+
+	public String message() {
+		return this.serviceProperties.getMessage();
+	}
+}
