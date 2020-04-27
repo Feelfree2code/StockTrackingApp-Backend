c81a5b616fd51574ec9cb57dd905f0dabadd7b6a
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-04-27 23:32:47 +0300

    All empty controller are created.

---
+++ src/main/java/com/Feelfree2code/STA/controller/ProjectPartController.java
@@ -0,0 +1,9 @@
+package com.Feelfree2code.STA.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/projectPart")
+public class ProjectPartController {
+}
